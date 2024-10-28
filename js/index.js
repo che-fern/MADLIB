2b10237968ca58581c7f8a7fa25cdd1f6462a4f9
CHELSEA <CEFERNAN@UVM.EDU>
2024-10-28 10:12:28 -0400

    commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const TypeOfBirdInput = document.getElementById('TypeOfBird');
+const TypeOfBird = TypeOfBirdInput.value;
+
+const NameInput = document.getElementById('Name');
+const Name = NameInput.value;
+
+const DayOfWeekInput = document.getElementById('DayOfWeek');
+const DayOfWeek = DayOfWeekInput.value;
+
+const GenerateMadLibs=()=> {console.log("working")}
